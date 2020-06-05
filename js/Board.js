c7bc1a7c41e0657da26fac673aa117467034d3be
Akira Kosuda <36807077+sayuen0@users.noreply.github.com>
2020-06-06 08:57:47 +0900

    add : board style

--- js/Board.js
+++ js/Board.js
@@ -1,4 +1,17 @@
+class Board extends DOM {
+  constructor(tagName, ...classes) {
+    super(tagName, ...classes);
+    console.log("board");
+    this.setStyle();
+  }
 
-class Board extends DOM{
-
+  /**
+   * ボードのスタイリングをする
+   */
+  setStyle() {
+    //1セル25px * 8
+    const size = "200px";
+    this._node.style.height = size;
+    this._node.style.width = size;
+  }
 }
