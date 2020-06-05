cf2c829d15d5080e1d05439cada28d22a6b1dc97
Akira Kosuda <36807077+sayuen0@users.noreply.github.com>
2020-06-06 08:57:40 +0900

    add: DOM rendering function

--- js/DOM.js
+++ js/DOM.js
@@ -3,5 +3,32 @@
  * HTMLへのレンダリングを行う
  */
 class DOM {
+  /**
+   * コンストラクタ
+   * タグとしてNodeを生成
+   * @param {String} tagName HTMLでのタグ名
+   */
+  constructor(tagName, ...classes) {
+    this._node = document.createElement(tagName);
+    if (classes.length > 0) {
+      classes.forEach((className) => {
+        this._node.classList.add(className);
+      })
+    }
+  }
+  /**
+   * 自身を子要素として付加する
+   * @param {DOM} target 付加対象
+   */
+  appendTo(target) {
+    target.appendChild(this._node);
+  }
 
+  /**
+   * デフォルトのスタイリングをする
+   */
+  setStyle() {
+
+  }
 }
+
