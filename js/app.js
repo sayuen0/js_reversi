c8f60abce8463dc421e4268c3620662ab30cbaf0
Akira Kosuda <36807077+sayuen0@users.noreply.github.com>
2020-06-06 08:57:25 +0900

    add: entry point to HTML

--- js/app.js
+++ js/app.js
@@ -6,5 +6,7 @@ main()
  */
 function main() {
   const root = document.getElementById("root");
-  const board = new Board();
+  const board = new Board("div", "board");
+  console.log(board);
+  board.appendTo(root);
 }
