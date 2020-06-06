480527ed3c656ceead0619fb869169a182f7ea09
Akira Kosuda <36807077+sayuen0@users.noreply.github.com>
2020-06-06 21:54:36 +0900

    add: cell#setStone

--- js/Cell.js
+++ js/Cell.js
@@ -3,25 +3,58 @@
  * 石をおける
  */
 class Cell extends DOM {
-  constructor() {
+  /**
+   * 横番地
+   */
+  _v = null;
+  /**
+   * 縦番地
+   */
+  _h = null;
+
+  _stone = null;
+  /**
+   * コンストラクタ
+   * セルを作成
+   * 番地を登録
+   * クリックイベントを登録
+   * 置かれた状態はfalse
+   */
+  constructor(v, h) {
     const tagName = "div";
     const classes = ["cell"];
     super(tagName, classes);
+    this._v = v;
+    this._h = h;
     //クリックイベント登録
-    this._node.addEventListener("click", this.sayHello);
+    this._node.addEventListener("click", (event) => {
+      //TODO: クリックした回数をカウントして、白と黒を置き分ける
+      const color = Math.random() <= 0.5 ? "black" : "white";
+      this.setStone(color);
+    });
+    this._alreadyPut = false;
   }
 
   /**
+   * 石を置く
+   * 置いた位置には新たにはおけない
+   * @param {String} color 石の色
    */
+  setStone(color) {
+    console.log("石が置かれました", this._v, this._h);
+    if (this._alreadyPut) {
+      return console.log("すでに置かれています");
+    }
+    this._stone = new Stone(color);
+    this.addChildNode(this._stone);
+    this._alreadyPut = true;
   }
 
   /**
-   * ダミーメソッド
-   * @param {Event} event 
+   * 石を裏返す
    */
-  sayHello(event) {
-    //TODO: メソッド名を適切なものに変える
-    //TODO: クリックされたらマスのインデックスを特定したい
-    console.log("Hello");
+  //TODO: あってもいいけどなくて良い
+  reverseStone() {
+    this.stone.reverse();
   }
 }
