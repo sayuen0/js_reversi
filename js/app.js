090f7ea9840cf3bf94a99fb625bd598be24d295e
Akira Kosuda <36807077+sayuen0@users.noreply.github.com>
2020-06-06 09:53:21 +0900

    update entrypoint

--- js/app.js
+++ js/app.js
@@ -1,12 +1,38 @@
+//ゲーム状態の初期化
 main()
-
+const gameStatus = new GameStatus();
 /**
  * ゲームの初期状態を作成する
  * 空のボードをレンダリングする
  */
 function main() {
   const root = document.getElementById("root");
-  const board = new Board("div", "board");
+  const board = new Board();
   console.log(board);
   board.appendTo(root);
+  const cellCount = 8;
+  const gameStatus = new GameStatus();
+  for (let x = 0; x < cellCount; x++) {
+    for (let y = 0; y < cellCount; y++) {
+      const cell = new Cell();
+      cell.appendTo(board);
+    }
+  }
+  setReady();
 }
+
+/**
+ * 中央4マスに最初のコマをセットする
+ */
+function setReady() {
+  const stone1 = new Stone("black");
+  const stone2 = new Stone("black");
+  const stone3 = new Stone("black");
+  const stone4 = new Stone("black");
+  const stones = [stone1, stone2, stone3, stone4];
+  
+  console.log(stone);
+}
+
+
+//TODO: マスがクリックされたらそこに石を置く
