e79edf86a624604336cdb0baab71a299514e7781
Akira Kosuda <36807077+sayuen0@users.noreply.github.com>
2020-06-06 09:50:41 +0900

    change: appendTo これだけHTMLタグに直接貼り付けるので、オーバーライド

--- js/Board.js
+++ js/Board.js
@@ -1,8 +1,12 @@
 class Board extends DOM {
-  constructor(tagName, ...classes) {
-    super(tagName, ...classes);
-    console.log("board");
-    this.setStyle();
+  /**
+   * コンストラクタ
+   * タグ名とクラス名を決定
+   */
+  constructor() {
+    const tagName = "div";
+    const classes = ["board"];
+    super(tagName, classes);
   }
 
   /**
@@ -10,8 +14,16 @@ class Board extends DOM {
    */
   setStyle() {
     //1セル25px * 8
-    const size = "200px";
+    const size = "404px";
     this._node.style.height = size;
     this._node.style.width = size;
+  }
+
+  /**
+   * 基底となるHTMLタグにゲーム版を貼り付ける
+   * @param {Node} root 基底となるHTMLタグそのもの
+   */
+  appendTo(root) {
+    root.appendChild(this._node);
   }
 }
