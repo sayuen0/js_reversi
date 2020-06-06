f72cbe9cba5b38c6f5b5642f1840a0075811358d
Akira Kosuda <36807077+sayuen0@users.noreply.github.com>
2020-06-06 09:53:10 +0900

    add: cell constructor

--- js/Cell.js
+++ js/Cell.js
@@ -1,6 +1,36 @@
 /**
- * 
+ * マス目クラス
+ * 石をおける
  */
-class Cell extends DOM{
+class Cell extends DOM {
+  constructor() {
+    const tagName = "div";
+    const classes = ["cell"];
+    super(tagName, classes);
+    //クリックイベント登録
+    this._node.addEventListener("click", this.sayHello);
+  }
 
+  /**
+   * 初期スタイル
+   */
+  setStyle() {
+    const size = "50px";
+    this._node.style.width = size;
+    this._node.style.height = size;
+    this._node.style.display = "inline-block";
+    this._node.style.lineHeight = "1";
+    this._node.style.fontSize = 0;
+    this._node.style.verticalAlign = "bottom"
+  }
+
+  /**
+   * ダミーメソッド
+   * @param {Event} event 
+   */
+  sayHello(event) {
+    //TODO: メソッド名を適切なものに変える
+    //TODO: クリックされたらマスのインデックスを特定したい
+    console.log("Hello");
+  }
 }
