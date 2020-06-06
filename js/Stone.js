5a6b1be1d459d0e34057e75170d3630fb2bd73e9
Akira Kosuda <36807077+sayuen0@users.noreply.github.com>
2020-06-06 09:52:57 +0900

    add: Stone constructor

--- js/Stone.js
+++ js/Stone.js
@@ -2,7 +2,22 @@
  * 石クラス
  */
 class Stone extends DOM {
+  /**
+   * 
+   * @param {String} color 
+   */
+  constructor(color) {
+    const tagName = "div";
+    //白でも黒でもどっちでも無いのが与えられたら白にしとく
+    if (color !== "white" || color !== "black") {
+      color = "white";
+    }
+    const classes = ["stone", color];
+    super(tagName, classes);
+  }
 
+  setStyle() {
+  }
 }
 
 
