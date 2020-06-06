e3f552702d1de26df19ca4916821b816ecddd26e
Akira Kosuda <36807077+sayuen0@users.noreply.github.com>
2020-06-06 22:45:49 +0900

    add : ゲーム状態にプレイヤー切り替えさせる

--- js/GameStatus.js
+++ js/GameStatus.js
@@ -1,24 +1,48 @@
 /**
  * ゲームの状態管理
+ * 各コンポーネントにおけるクリックイベントのオブザーバー
  */
 class GameStatus {
   /**
-   * ゲーム盤
+   * プレイヤーたち
    */
-  _board = null;
+  _players = [];
 
   /**
-   * ターンプレイヤー
+   * 次の手番プレイヤー
    */
+  _turnPlayer = null;
+
 
   /**
    * コンストラクタ
    * ゲーム盤をセット
-   * @param {Board} board ゲーム盤
+   * プレイヤーを2人セット
    */
-  constructor(board) {
-    this._board = board;
+  constructor() {
+    const user = new Player("white", "あなた");
+    const opponent = new Player("black", "CPU");
+    this._players.push(user);
+    this._players.push(opponent);
     this.selectFirstHand();
   }
 
+  /**
+   * 先手を決める
+   */
+  selectFirstHand() {
+    //TODO:  ランダムないしはじゃんけんの結果で決められるようにする
+    //とりあえずあなた
+    this._turnPlayer = this._players[0];
+    console.log(this._turnPlayer.name + "の番です");
+  }
+
+  /**
+   * 石が置かれたことでアクションを取る
+   * ターンプレイヤーを切り替える
+   */
+  observeStonePut() {
+    this._turnPlayer = this._turnPlayer === this._players[0] ? this._players[1] : this._players[0];
+    console.log(this._turnPlayer.name + "の番です");
+  }
 }
