7a3c1b9ce3aacea0a210d68185396958efb7dfce
Akira Kosuda <36807077+sayuen0@users.noreply.github.com>
2020-06-06 21:51:19 +0900

    add: ゲーム初期状態

--- js/Board.js
+++ js/Board.js
@@ -1,12 +1,29 @@
+/**
+ * ゲーム盤
+ * 8x8のマス目を持つ
+ */
 class Board extends DOM {
+
+  /**
+   * マス目たち
+   */
+  _cellList = null;
+
   /**
    * コンストラクタ
    * タグ名とクラス名を決定
+   * HTML基底タグに貼り付ける
+   * 空の8x8マスを作成して付加
+   * 最初の4つの石を配置
+   * @param {DOM} root 基底タグ
    */
-  constructor() {
+  constructor(root) {
     const tagName = "div";
     const classes = ["board"];
     super(tagName, classes);
+    this.appendTo(root);
+    this.createEmptyCellsList();
+    this.setReady();
   }
 
   /**
@@ -15,5 +32,47 @@ class Board extends DOM {
    */
   appendTo(root) {
     root.appendChild(this._node);
+  }
+
+  /**
+   * ゲーム開始時の初期処理としてからの8x8マスを作成する。
+   * ボードに順に貼り付ける
+   */
+  createEmptyCellsList() {
+    const cellCount = 8
+    this._cellList = new Array(cellCount);
+    for (let v = 0; v < cellCount; v++) {
+      //空っぽなら、横列をを作成する
+      if (this._cellList[v] === null || this._cellList[v] === undefined) {
+        this._cellList[v] = new Array(cellCount);
+      }
+      for (let h = 0; h < cellCount; h++) {
+        const cell = new Cell(v, h);
+        this.addChildNode(cell);
+        this._cellList[v][h] = cell;
+      }
+    }
+  }
+  /**
+   * ゲーム開始状態として4つの石をセットする
+   */
+  setBeginningStones() {
+  }
+
+  /**
+   * ゲーム開始状態を作成する
+   * 白、黒、白、黒を初期配置する
+   */
+  setReady() {
+    const stoneColors = ["white", "black", "black", "white"];
+    const cells = [
+      this._cellList[3][3],
+      this._cellList[3][4],
+      this._cellList[4][3],
+      this._cellList[4][4],
+    ]
+    stoneColors.forEach((color, index) => {
+      cells[index].setStone(color);
+    })
   }
 }
--- js/GameStatus.js
+++ js/GameStatus.js
@@ -3,34 +3,22 @@
  */
 class GameStatus {
   /**
-   * コンストラクタ
-   * 石を入れるマスの二次元配列を初期化する
+   * ゲーム盤
    */
-  constructor() {
-    const cellCount = 8;
-    const cells = new Array(cellCount);
-    for (let y = 0; y < cellCount; y++) {
-      cells[y] = new Array(cellCount);
-    }
-    this._cellList = cells;
-  }
+  _board = null;
 
   /**
-     * 
-     * @param {int} x 横インデックス
-     * @param {int} y 縦インデックス
-     * @param {Cell} cell マス
+   * ターンプレイヤー
    */
-  setCell(x, y, cell) {
-    this._cellList[x][y] = cell;
-  }
+
   /**
-   * 
-   * @param {int} x 横インデックス
-   * @param {int} y 縦インデックス
-   * @param {Stone} stone 石
+   * コンストラクタ
+   * ゲーム盤をセット
+   * @param {Board} board ゲーム盤
    */
-  setStone(x, y, stone) {
-    stone.appendTo(this._cellList[x][y]);
+  constructor(board) {
+    this._board = board;
+    this.selectFirstHand();
   }
+
 }
