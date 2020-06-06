4c0f1ad99bf3ad2ac35b2a3995e3ff65f4b5cc0c
Akira Kosuda <36807077+sayuen0@users.noreply.github.com>
2020-06-06 21:49:30 +0900

    add: player.setStoneColor

--- js/Player.js
+++ js/Player.js
@@ -3,5 +3,23 @@
  * ユーザーかコンピュータ
  */
 class Player {
+  /**
+   * 打つ石の色
+   */
+  #color = null;
 
+  /**
+   * コンストラクタ
+   */
+  constructor() {
+
+  }
+
+  /**
+   * 打つ石の色をセットする
+   * @param {String} color 色
+   */
+  set color(color) {
+    this._color = color;
+  }
 }
