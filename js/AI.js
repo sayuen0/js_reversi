1700c943163242661263651701aef5b43688fc4a
Akira Kosuda <36807077+sayuen0@users.noreply.github.com>
2020-06-06 21:37:36 +0900

    add: AI class

---
+++ js/AI.js
@@ -0,0 +1,28 @@
+/**
+ * 人工知能
+ * 一人プレイ用対戦相手コンピュータ
+ */
+class AI {
+
+}
+
+/**
+ * かんたんAI
+ */
+class WeakAI extends AI {
+
+}
+
+/**
+ * 中くらいAI
+ */
+class MiddleAI extends AI {
+
+}
+
+/**
+ * 強いAI
+ */
+class StrongAI extends AI {
+
+}
