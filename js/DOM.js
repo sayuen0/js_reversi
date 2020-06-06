80e9b1ddba769db7e5fb781c203b729a8c541503
Akira Kosuda <36807077+sayuen0@users.noreply.github.com>
2020-06-06 09:52:20 +0900

    add: DOM.appendTo
    子クラス全部これでいける

--- js/DOM.js
+++ js/DOM.js
@@ -7,21 +7,23 @@ class DOM {
    * コンストラクタ
    * タグとしてNodeを生成
    * @param {String} tagName HTMLでのタグ名
+   * @param {Array} classes クラス名
    */
-  constructor(tagName, ...classes) {
+  constructor(tagName, classes = []) {
     this._node = document.createElement(tagName);
     if (classes.length > 0) {
       classes.forEach((className) => {
         this._node.classList.add(className);
       })
     }
+    this.setStyle();
   }
   /**
    * 自身を子要素として付加する
    * @param {DOM} target 付加対象
    */
   appendTo(target) {
-    target.appendChild(this._node);
+    target._node.appendChild(this._node);
   }
 
   /**
