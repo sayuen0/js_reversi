1e0beb72b76ac1db5fbe39f8fbc9e6d204e09902
Akira Kosuda <36807077+sayuen0@users.noreply.github.com>
2020-06-14 13:04:03 +0900

    add card testcase

---
+++ __tests__/trump/Card.spec.ts
@@ -0,0 +1,12 @@
+import { Card, CardSuit, CardNumber } from "../../src/trump/Card";
+
+
+describe('カード番号やカードマーク', () => {
+  it('カード番号が正しい', () => {
+    const card1 = new Card(CardSuit.Club, CardNumber.ACE);
+    expect(card1.suit).toBe(CardSuit.Club);
+    expect(card1.number).toBe(CardNumber.ACE)
+  });
+
+});
+
