cb05b31026e2e06afee40d5de24105e5e7b962f8
Akira Kosuda <36807077+sayuen0@users.noreply.github.com>
2020-06-14 22:30:04 +0900

    main整理

--- src/main.ts
+++ src/main.ts
@@ -3,28 +3,28 @@ import "../sass/style.scss";
 
 import { Card, Suit, Number } from "./trump/Card";
 import { CardList } from "./trump/CardList";
-import { HandTable } from "./trump/HandTable";
+import { HandTable, HandType } from "./trump/HandTable";
 
-const c1 = new Card(Suit.CLUB, Number.EIGHT);
-const c2 = new Card(Suit.CLUB, Number.NINE);
 const root = document.getElementById("root");
 if (!root) {
   throw new Error("rootがありません");
 }
-const deck = CardList.createNotShuffledDeck();
+// const deck = CardList.createNotShuffledDeck();
 //これを山札にするので、クラス名を与えて裏向き画像にしておくれ
-root.appendChild(deck.node);
-deck.shuflle(10000);
 let card: Card;
 const count = 5;
-const hands: CardList = new CardList();
+const deck = CardList.createNotShuffledDeck();
+deck.shuflle(10000);
+const hands = new CardList();
 for (let i = 0; i < count; i++) {
+  root.appendChild(deck.node);
   card = deck.draw();
   hands.add(card);
 }
-
 const table = new HandTable(hands);
 root.appendChild(table.node);
 table.node.appendChild(hands.node);
-console.log(table.judgeStrength());
+const result = table.judgeStrength();
+console.log(result);
+root.appendChild(document.createTextNode(result.name));
 
